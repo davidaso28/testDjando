df1f1404db068fcd27c729363db90737c2b8c8b6
davidaso28 <da.saavedra@uniandes.edu.co>
2017-06-20 22:28:19 -0500

    primera migracion creación de los modelos

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Persona (models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    domicilio = models.TextField()
