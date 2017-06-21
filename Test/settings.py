0011cac5bc4f539e26af7e6cc9bafcae97c06af9
davidaso28 <da.saavedra@uniandes.edu.co>
2017-06-20 21:52:41 -0500

    se agregan apps e idioma

--- Test/settings.py
+++ Test/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.adopcion',
+    'apps.mascota',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
