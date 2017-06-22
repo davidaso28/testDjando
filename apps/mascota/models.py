f744fe2d549fd0bc38f93c25595d424c93ddf4f6
davidaso28 <da.saavedra@uniandes.edu.co>
2017-06-21 22:24:37 -0500

    Se agregan más atributos a los modelos (relaciones)

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from apps.adopcion.models import Persona
 # Create your models here.
+class Vacuna(models.Model):
+    nombre=models.CharField(max_length=50)
 class Mascota (models.Model):
-    folio = models.CharField(primary_key=True, max_length=10)
     nombre = models.CharField(max_length=50)
     sexo = models.CharField(max_length=10)
     edad_aproximada = models.IntegerField()
     fecha_rescate = models.DateField()
+    person=models.ForeignKey(Persona,null=True,blank=True, on_delete=models.CASCADE)
+    vacuna=models.ManyToManyField(Vacuna, blank=True)
